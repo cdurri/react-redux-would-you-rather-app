ac138d30b96d7df209312b6fed4338572380ec5a
Cian <cian.durrigan@gmail.com>
2019-09-09 14:57:14 +0100

    add markup for header navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container, Row, Col } from 'reactstrap'
+import Header from './Header'
 import '../index.css';
 
 function App() {
   return (
-    <div>
-      Would You Rather?
-    </div>
+    <Router>
+        <Header />
+    </Router>
   );
 }
 
---
+++ src/components/Navigation.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { Navbar, NavbarToggler, Nav, NavbarBrand, NavItem } from 'reactstrap'
+import { Container, Row, Col } from 'reactstrap'
+
+export default function Navigation () {
+  return (
+    <Navbar color='light' className='nav'>
+        <Nav>
+          <NavItem>
+            <NavLink to='/' exact activeClassName='active'>
+              Home
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to='/new-question' activeClassName='active'>
+              New Question
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to='leaderboard' activeClassName='active'>
+              Leaderboard
+            </NavLink>
+          </NavItem>
+        </Nav>
+    </Navbar>
+  )
+}
