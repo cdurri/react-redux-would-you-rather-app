b3d102e006310b1f17963f96a9706405d09c1d7c
Cian <cian.durrigan@gmail.com>
2019-09-10 17:59:10 +0100

    Populate select dropdown with users names from Redux store

--- src/components/SignInCard.js
+++ src/components/SignInCard.js
@@ -1,34 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { Card, Button, CardHeader, CardBody, CardImg, CardTitle, CardText, Form, FormGroup, Label, Input } from 'reactstrap'
 import { DiReact } from 'react-icons/di'
 import Logo from '../images/react.png'
 
-const SignInCard = () => {
-  return (
-    <div>
-      <Card>
-        <CardHeader>
-          Welcome to the Would You Rather App!
-          <CardText>Please sign in to continue</CardText>
-          <CardBody>
-            <CardImg src={Logo} />
-            <Form>
-              <FormGroup>
-                <Label for='selectUser'>Sign In</Label>
-                <Input type='select' name='select' id='selectUser'>
-                  <option value=''>Select User</option>
-                  <option>Sarah Edo</option>
-                  <option>Tyler McGinnis</option>
-                  <option>John Doe</option>
-                </Input>
-              </FormGroup>
-              <Button>Sign In</Button>
-            </Form>
-          </CardBody>
-        </CardHeader>
-      </Card>
-    </div>
-  )
+class SignInCard extends Component {
+
+  render() {
+    const { users } = this.props
+
+    console.log(this.props.users)
+
+    const { name } = users
+
+    console.log(name)
+
+    return (
+      <div>
+        <Card>
+          <CardHeader>
+            Welcome to the Would You Rather App!
+            <CardText>Please sign in to continue</CardText>
+            <CardBody>
+              <CardImg src={Logo} />
+              <Form>
+                <FormGroup>
+                  <Label for='selectUser'>Sign In</Label>
+                  <Input type='select' name='select' id='selectUser'>
+                    <option value=''>Select User</option>
+                    {this.props.users.map((user) => (
+                      <option>{user}</option>
+                    ))}
+                  </Input>
+                </FormGroup>
+                <Button>Sign In</Button>
+              </Form>
+            </CardBody>
+          </CardHeader>
+        </Card>
+      </div>
+    )
+  }
 }
 
-export default SignInCard
+function mapStateToProps({ users }, { name }) {
+  const user = users[name]
+
+  return {
+    users: Object.keys(users)
+      .map((user) => users[user].name)
+  }
+}
+
+export default connect(mapStateToProps)(SignInCard)
