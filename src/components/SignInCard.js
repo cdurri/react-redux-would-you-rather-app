3597f3add9c4c48209a3ab600f7feadbd36c0135
Cian <cian.durrigan@gmail.com>
2019-09-11 15:28:47 +0100

    Add styling to login card

--- src/components/SignInCard.js
+++ src/components/SignInCard.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Card, Button, CardHeader, CardBody, CardImg, CardTitle, CardText, Form, FormGroup, Label, Input } from 'reactstrap'
+import { Container, Row, Col, Card, Button, CardHeader, CardBody, CardImg, CardTitle, CardText, Form, FormGroup, Label, Input } from 'reactstrap'
 import { DiReact } from 'react-icons/di'
 import Logo from '../images/react.png'
 import { setAuthedUser } from '../actions/authedUser'
@@ -33,27 +33,38 @@ class SignInCard extends Component {
 
     return (
       <div>
-        <Card>
-          <CardHeader>
-            Welcome to the Would You Rather App!
-            <CardText>Please sign in to continue</CardText>
-            <CardBody>
-              <CardImg src={Logo} />
-              <Form onSubmit={this.handleSignIn}>
-                <FormGroup>
-                  <Label for='selectUser'>Sign In</Label>
-                  <Input type='select' name='select' id='selectUser' onChange={this.handleChange}>
-                    <option value=''>Select User</option>
-                    {this.props.users.map((user) => (
-                      <option key={user.id} value={user.id}>{user.name}</option>
-                    ))}
-                  </Input>
-                </FormGroup>
-                <Button className='sign-in' type='submit' disabled={signIn === false}>Sign In</Button>
-              </Form>
-            </CardBody>
-          </CardHeader>
-        </Card>
+      <Container>
+        <Row>
+          <Col sm="12" md={{ size: 8, offset: 2}}>
+            <Card className='login-card'>
+              <CardHeader>
+                Welcome to the Would You Rather App!
+                <CardText>Please sign in to continue</CardText>
+              </CardHeader>
+              <CardBody>
+                <CardImg src={Logo} />
+                <Form onSubmit={this.handleSignIn}>
+                  <FormGroup>
+                    <Label for='selectUser'>Sign in</Label>
+                    <Input type='select' name='select' id='selectUser' onChange={this.handleChange}>
+                      <option value=''>Select User</option>
+                      {users.map((user) => (
+                        <option key={user.id} value={user.id}>
+                          <img
+                            src={user.avatar} /* todo: come back  to get avatar to show properly */
+                          />
+                          {user.name}
+                        </option>
+                      ))}
+                    </Input>
+                  </FormGroup>
+                  <Button className='btn-sign-in' type='submit' disabled={signIn === false}>Sign In</Button>
+                </Form>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
       </div>
     )
   }
@@ -64,8 +75,9 @@ function mapStateToProps({ users }) {
     users: Object.keys(users)
       .map((user) => ({
         user,
+        avatar: users[user].avatarURL,
         name: users[user].name,
-        id: users[user].id,
+        id: users[user].id
       }))
   }
 }
