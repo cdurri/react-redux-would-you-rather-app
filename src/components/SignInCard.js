843590c5919bcf023f3e200b980a4b8a499ecd38
Cian <cian.durrigan@gmail.com>
2019-09-11 12:10:43 +0100

    add login functionality using event listeners to set authedUser

--- src/components/SignInCard.js
+++ src/components/SignInCard.js
@@ -3,17 +3,35 @@ import { connect } from 'react-redux'
 import { Card, Button, CardHeader, CardBody, CardImg, CardTitle, CardText, Form, FormGroup, Label, Input } from 'reactstrap'
 import { DiReact } from 'react-icons/di'
 import Logo from '../images/react.png'
+import { setAuthedUser } from '../actions/authedUser'
 
 class SignInCard extends Component {
 
+  state = {
+    signIn: false,
+    userId: ''
+  }
+
+  handleChange = (e) => {
+    e.preventDefault()
+
+    console.log(e.target.value)
+
+    this.setState({signIn: true, userId: e.target.value})
+  }
+
   handleSignIn = (e) => {
-    {/* todo: handle when sign in button is clicked  */}
+    e.preventDefault()
+
+    this.props.dispatch(setAuthedUser(this.state.userId))
+
+    //console.log(e.target.value)
   }
 
   render() {
     const { users } = this.props
 
-    const { name } = users
+    const { signIn } = this.state
 
     return (
       <div>
@@ -23,17 +41,17 @@ class SignInCard extends Component {
             <CardText>Please sign in to continue</CardText>
             <CardBody>
               <CardImg src={Logo} />
-              <Form>
+              <Form onSubmit={this.handleSignIn}>
                 <FormGroup>
                   <Label for='selectUser'>Sign In</Label>
-                  <Input type='select' name='select' id='selectUser'>
+                  <Input type='select' name='select' id='selectUser' onChange={this.handleChange}>
                     <option value=''>Select User</option>
                     {this.props.users.map((user) => (
-                      <option>{user}</option>
+                      <option key={user.id} value={user.id}>{user.name}</option>
                     ))}
                   </Input>
                 </FormGroup>
-                <Button>Sign In</Button>
+                <Button className='sign-in' type='submit' disabled={signIn === false}>Sign In</Button>
               </Form>
             </CardBody>
           </CardHeader>
@@ -43,12 +61,14 @@ class SignInCard extends Component {
   }
 }
 
-function mapStateToProps({ users }, { name }) {
-  const user = users[name]
-
+function mapStateToProps({ users }) {
   return {
     users: Object.keys(users)
-      .map((user) => users[user].name)
+      .map((user) => ({
+        user,
+        name: users[user].name,
+        id: users[user].id,
+      }))
   }
 }
 
