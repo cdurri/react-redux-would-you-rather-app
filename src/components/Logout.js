2ae356a519146e3292c61a9a5e512d258629eaea
Cian <cian.durrigan@gmail.com>
2019-09-12 12:08:47 +0100

    Add functionality to show logged in users avatar

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Container, Row, Col } from 'reactstrap'
+import { Avatar } from '@material-ui/core'
 import { setAuthedUser } from '../actions/authedUser'
 
 class Logout extends Component {
@@ -12,13 +13,12 @@ class Logout extends Component {
   }
 
   render() {
-
     return (
-      <div className='logged-in'>
-        <span>Hello, {this.props.user}</span>
+      <div className='logout-panel'>
+        <span>Hello, {this.props.username}</span>
         <span>
-          <img
-            src='https://placeimg.com/30/30/people'
+          <Avatar
+            src={this.props.useravatar}
             className='logged-in-avatar'
             alt='user avatar'
           />
@@ -29,9 +29,12 @@ class Logout extends Component {
   }
 }
 
-function mapStateToProps({ authedUser }) {
+function mapStateToProps({ authedUser, users }) {
   return {
-    user: authedUser
+    username: users[authedUser].name,
+    useravatar: authedUser
+      ? users[authedUser].avatarURL
+      : null
   }
 }
 
