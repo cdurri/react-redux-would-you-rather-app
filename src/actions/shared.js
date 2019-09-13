6f8b3180df7c133be44019f60c1af94cb8fde6e3
Cian <cian.durrigan@gmail.com>
2019-09-13 13:08:35 +0100

    Pull in questions data from database to the Redux store

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,14 +1,16 @@
 import { getInitialData } from '../utils/api'
 import { receiveUsers } from '../actions/users'
 import { setAuthedUser } from '../actions/authedUser'
+import { receiveQuestions } from '../actions/questions'
 
 const AUTHED_ID = null
 
 export function handleInitialData () {
   return (dispatch) => {
     return getInitialData()
-      .then(({ users }) => {
+      .then(({ users, questions }) => {
         dispatch(receiveUsers(users))
+        dispatch(receiveQuestions(questions))
         dispatch(setAuthedUser(AUTHED_ID))
       })
   }
