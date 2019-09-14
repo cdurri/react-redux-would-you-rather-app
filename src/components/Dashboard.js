52a2451f8decec4822acc43d6dc2dd35c68ea05f
Cian <cian.durrigan@gmail.com>
2019-09-14 12:29:13 +0100

    Sort questions by timestamp for unanswered questions

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -24,59 +24,66 @@ class Dashboard extends Component {
 
     return (
       <div>
-      <Container>
-        <Row>
-          <Col sm="12" md={{ size: 8, offset: 2}}>
-            <Nav tabs>
-              <NavItem>
-                <NavLink
-                  className={classnames({ active: this.state.activeTab === '1' })}
-                  onClick={() => {this.toggle('1') }}
-                >
-                  Unanswered Questions
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  className={classnames({ active: this.state.activeTab === '2' })}
-                  onClick={() => {this.toggle('2') }}
-                >
-                  Answered Questions
-                </NavLink>
-              </NavItem>
-            </Nav>
-            <TabContent activeTab={this.state.activeTab}>
-              <TabPane tabId='1'>
-                <Row>
-                  <Col sm='12'>
-                    <ListGroup>
-                      {numbers.map(num => (
-                        <ListGroupItem>
-                          <QuestionCard />
-                        </ListGroupItem>
-                      ))}
-                    </ListGroup>
-                  </Col>
-                </Row>
-              </TabPane>
-              <TabPane tabId='2'>
-                <Row>
-                  <Col sm='12'>
-                    <ListGroup>
-                      {numbers.map(num => (
-                        <ListGroupItem>{num * 2}</ListGroupItem>
-                      ))}
-                    </ListGroup>
-                  </Col>
-                </Row>
-              </TabPane>
-            </TabContent>
-          </Col>
-        </Row>
-      </Container>
+        <Container>
+          <Row>
+            <Col sm="12" md={{ size: 8, offset: 2}}>
+              <Nav tabs>
+                <NavItem>
+                  <NavLink
+                    className={classnames({ active: this.state.activeTab === '1' })}
+                    onClick={() => {this.toggle('1') }}
+                  >
+                    Unanswered Questions
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink
+                    className={classnames({ active: this.state.activeTab === '2' })}
+                    onClick={() => {this.toggle('2') }}
+                  >
+                    Answered Questions
+                  </NavLink>
+                </NavItem>
+              </Nav>
+              <TabContent activeTab={this.state.activeTab}>
+                <TabPane tabId='1'>
+                  <Row>
+                    <Col sm='12'>
+                      <ListGroup>
+                        {this.props.questionIds.map((id) => (
+                          <ListGroupItem key={id}>
+                            <QuestionCard id={id} />
+                          </ListGroupItem>
+                        ))}
+                      </ListGroup>
+                    </Col>
+                  </Row>
+                </TabPane>
+                <TabPane tabId='2'>
+                  <Row>
+                    <Col sm='12'>
+                      <ListGroup>
+                        {numbers.map(num => (
+                          <ListGroupItem>{num * 2}</ListGroupItem>
+                        ))}
+                      </ListGroup>
+                    </Col>
+                  </Row>
+                </TabPane>
+              </TabContent>
+            </Col>
+          </Row>
+        </Container>
       </div>
     )
   }
 }
 
-export default connect()(Dashboard)
+function mapStateToProps({ questions }) {
+  return {
+    questionIds: Object.keys(questions)
+      .sort((a, b) => questions[b].timestamp - questions[a].timestamp)
+  }
+}
+
+export default connect(mapStateToProps)(Dashboard)
--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -1,12 +1,40 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
+import { Container, Row, Col, Card, Button, CardHeader, CardBody, CardImg, CardTitle, CardText, Form, FormGroup, Label, Input } from 'reactstrap'
 
-class QuestionCard extends Component {
-  render() {
-    return (
-      <div>Question Card</div>
-    )
+const QuestionCard = props => {
+
+  const { id } = props
+
+  const { author } = props
+
+  console.log(props.question)
+
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col sm="12" md={{ size: 12, offset: 0}}>
+            <Card className='question-card'>
+              <CardHeader>
+                {author} asks:
+              </CardHeader>
+              <CardBody>
+
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  )
+}
+
+
+function mapStateToProps({ questions, users }, { id }) {
+  return {
+    author: questions[id].author
   }
 }
 
-export default connect()(QuestionCard)
+export default connect(mapStateToProps)(QuestionCard)
