f3bab12f32178a3941bcae8ad7b660f4712902c5
Cian <cian.durrigan@gmail.com>
2019-09-21 21:43:30 +0100

    Add event listeners to enable submit button on radio change

--- src/components/PollPage.js
+++ src/components/PollPage.js
@@ -3,63 +3,75 @@ import { connect } from 'react-redux'
 import { Container, Row, Col, Card, Button, CardHeader, CardBody, CardImg, CardTitle, CardText, Form, FormGroup, Label, Input } from 'reactstrap'
 import { Avatar } from '@material-ui/core'
 
-const PollPage = props => {
+class PollPage extends Component {
 
-    const { author, avatar, optionOne, optionTwo, id } = props
+    state = {
+      submit: false
+    }
 
-    console.log('ID: ', id)
-    console.log('OptionOne: ', optionOne)
-    console.log('OptionTwo: ', optionTwo)
+    handleChange = (e) => {
+      this.setState({submit: true})
+    }
 
-    return (
+    render() {
 
-      <div>
-        <Container className='poll-card'>
-          <Row>
-            <Col sm="12" md={{ size: 8, offset: 2}}>
-              <Card className='question-card'>
-                <CardHeader>
-                  {author} asks:
-                </CardHeader>
-                <CardBody>
-                  <Row>
-                    <Col sm='4' className='poll-card__avatar'>
-                      <Avatar
-                        src={avatar}
-                        className='poll-card__avatar-image'
-                        alt='User avatar'
-                      />
-                    </Col>
-                    <Col sm='8' className='question-card__options-preview'>
-                      <CardTitle className='question-card__title'>Would you rather</CardTitle>
-                      <Form>
-                        <FormGroup tag='fieldset'>
-                          <FormGroup check>
-                            <Label check>
-                              <Input type='radio' name='radio1' />{' '}
-                                {optionOne}
-                            </Label>
+      const { author, avatar, optionOne, optionTwo, id } = this.props
+
+      const { submit } = this.state
+
+      console.log('ID: ', id)
+      console.log('OptionOne: ', optionOne)
+      console.log('OptionTwo: ', optionTwo)
+
+      return (
+        <div>
+          <Container className='poll-card'>
+            <Row>
+              <Col sm="12" md={{ size: 8, offset: 2}}>
+                <Card className='question-card'>
+                  <CardHeader>
+                    {author} asks:
+                  </CardHeader>
+                  <CardBody>
+                    <Row>
+                      <Col sm='4' className='poll-card__avatar'>
+                        <Avatar
+                          src={avatar}
+                          className='poll-card__avatar-image'
+                          alt='User avatar'
+                        />
+                      </Col>
+                      <Col sm='8' className='question-card__options-preview'>
+                        <CardTitle className='question-card__title'>Would you rather</CardTitle>
+                        <Form>
+                          <FormGroup tag='fieldset'>
+                            <FormGroup check>
+                              <Label check>
+                                <Input type='radio' name='radio1' onChange={this.handleChange} />{' '}
+                                  {optionOne}
+                              </Label>
+                            </FormGroup>
+                            <FormGroup check>
+                              <Label check>
+                                <Input type='radio' name='radio1' onChange={this.handleChange} />{' '}
+                                  {optionTwo}
+                              </Label>
+                            </FormGroup>
                           </FormGroup>
-                          <FormGroup check>
-                            <Label check>
-                              <Input type='radio' name='radio1' />{' '}
-                                {optionTwo}
-                            </Label>
-                          </FormGroup>
-                        </FormGroup>
-                      </Form>
-                      <Button className='question-card__view-poll-btn'>
-                        Submit
-                      </Button>
-                    </Col>
-                  </Row>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    )
+                        </Form>
+                        <Button className='question-card__view-poll-btn' disabled={submit === false}>
+                          Submit
+                        </Button>
+                      </Col>
+                    </Row>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      )
+  }
 }
 
 function mapStateToProps({ questions, users }, props) {
